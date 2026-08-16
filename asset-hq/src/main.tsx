819fbd5a8833916f23355ff51113aa33c15ded70
COSMICLEX <141687289+hyeonseochoi2006@users.noreply.github.com>
2026-08-16 17:57:27 +0900

    Load CEO approval queue styles

--- asset-hq/src/main.tsx
+++ asset-hq/src/main.tsx
@@ -6,6 +6,7 @@ import './styles.css'
 import './office.css'
 import './three.css'
 import './daily.css'
+import './approval.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
